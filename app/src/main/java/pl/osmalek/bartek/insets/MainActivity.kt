6428630702e65b7f20c7646f69fbdf2233fdd4fb
nonda <nonda95@gmail.com>
2021-03-30 22:57:50 +0200

    Add edit text for keyboard and toggle system bars button

--- app/src/main/java/pl/osmalek/bartek/insets/MainActivity.kt
+++ app/src/main/java/pl/osmalek/bartek/insets/MainActivity.kt
@@ -1,12 +1,12 @@
 package pl.osmalek.bartek.insets
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.WindowInsetsController
+import android.view.View.*
+import android.view.WindowInsets
+import android.view.WindowInsetsController.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.updatePadding
-import androidx.core.view.updatePaddingRelative
 import pl.osmalek.bartek.insets.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +15,9 @@ class MainActivity : AppCompatActivity() {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false)
         } else {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    SYSTEM_UI_FLAG_LAYOUT_STABLE
         }
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -57,6 +57,32 @@ class MainActivity : AppCompatActivity() {
                     bottom = insets.systemWindowInsetBottom
                 )
                 insets
+            }
+            toggleSystemUi.setOnClickListener {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+                    window.insetsController?.run {
+                        when (systemBarsBehavior) {
+                            BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE -> {
+                                systemBarsBehavior = BEHAVIOR_SHOW_BARS_BY_TOUCH
+                                show(WindowInsets.Type.systemBars())
+                            }
+                            BEHAVIOR_SHOW_BARS_BY_SWIPE -> {
+                                systemBarsBehavior = BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                                hide(WindowInsets.Type.systemBars())
+                            }
+                            BEHAVIOR_SHOW_BARS_BY_TOUCH -> {
+                                systemBarsBehavior = BEHAVIOR_SHOW_BARS_BY_SWIPE
+                                hide(WindowInsets.Type.systemBars())
+                            }
+                        }
+                    }
+                } else with(window.decorView) {
+                    systemUiVisibility = when {
+                        systemUiVisibility and SYSTEM_UI_FLAG_IMMERSIVE_STICKY == SYSTEM_UI_FLAG_IMMERSIVE_STICKY -> systemUiVisibility xor SYSTEM_UI_FLAG_IMMERSIVE_STICKY xor SYSTEM_UI_FLAG_FULLSCREEN xor SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        systemUiVisibility and SYSTEM_UI_FLAG_IMMERSIVE == SYSTEM_UI_FLAG_IMMERSIVE -> (systemUiVisibility xor SYSTEM_UI_FLAG_IMMERSIVE) or SYSTEM_UI_FLAG_IMMERSIVE_STICKY or SYSTEM_UI_FLAG_FULLSCREEN or SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        else -> systemUiVisibility or SYSTEM_UI_FLAG_IMMERSIVE or SYSTEM_UI_FLAG_FULLSCREEN or SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    }
+                }
             }
         }
     }
